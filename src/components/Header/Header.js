a0a7711e3d3389ec74429154fb028cc1560b4531
MariaRez <maria.v.reznik@yandex.ru>
2023-09-13 17:36:22 +0200

    feat: start work with header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,38 @@
 import "./Header.css";
-import Logo from '../Logo/Logo';
+import Logo from "../Logo/Logo";
+import menu from "../../images/menu-logo.svg";
+import links from "../../images/links-logo.svg";
 
 function Header() {
+  function showMenu () {
+    console.log("открыли меню")
+  }
+
+  function showLinks () {
+    console.log("открыли ссылки")
+  }
+
   return (
     <header className="header text_size_xs additional-font" id="top">
       <Logo />
-      <nav>
-        <ul className="header__menu">
-          <li>Home</li>
-          <li>Pages</li>
-          <li>Contact Us</li>
-          <li>Blog</li>
-          <li>Landing</li>
-        </ul>
-      </nav>
-      <div className="header__links">
-        <p>Log in / Registration</p>
-        <p>Book table</p>
+      <div>
+        <img className="header__icons" src={menu} alt="menu" onClick={showMenu}/>
+        <nav>
+          <ul className="header__menu">
+            <li>Home</li>
+            <li>Pages</li>
+            <li>Contact Us</li>
+            <li>Blog</li>
+            <li>Landing</li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <img className="header__icons" src={links} alt="links" onClick={showLinks}/>
+        <div className="header__links">
+          <p>Log in / Registration</p>
+          <p>Book table</p>
+        </div>
       </div>
     </header>
   );
