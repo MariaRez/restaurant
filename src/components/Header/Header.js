5ea539e2ddf33d036d4bbcf6840a04eaad883428
MariaRez <maria.v.reznik@yandex.ru>
2023-10-12 12:43:28 +0200

    feat: links in header on medium size window

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,23 +2,27 @@ import "./Header.css";
 import Logo from "../Logo/Logo";
 import menu from "../../images/menu-logo.svg";
 import links from "../../images/links-logo.svg";
+import { useState } from "react";
 
 function Header() {
-  function showMenu () {
-    console.log("открыли меню")
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [linksVisible, setLinksVisible] = useState(false);
+
+  function toggleMenu() {
+    setMenuVisible((prevMenuVisible) => !prevMenuVisible);
   }
 
-  function showLinks () {
-    console.log("открыли ссылки")
+  function toggleLinks () {
+    setLinksVisible((prevLinksVisible) => !prevLinksVisible);
   }
 
   return (
     <header className="header text_size_xs additional-font" id="top">
       <Logo />
-      <div>
-        <img className="header__icons" src={menu} alt="menu" onClick={showMenu}/>
+      <div className="header__container">
+        <img className="header__icons" src={menu} alt="menu" onClick={toggleMenu}/>
         <nav>
-          <ul className="header__menu">
+          <ul className={`header__menu ${menuVisible ? 'header__menu_visible' : ''}`}>
             <li>Home</li>
             <li>Pages</li>
             <li>Contact Us</li>
@@ -27,9 +31,9 @@ function Header() {
           </ul>
         </nav>
       </div>
-      <div>
-        <img className="header__icons" src={links} alt="links" onClick={showLinks}/>
-        <div className="header__links">
+      <div className="header__container"> 
+        <img className="header__icons" src={links} alt="links" onClick={toggleLinks}/>
+        <div className={`header__links ${linksVisible ? 'header__links_visible' : ''}`}>
           <p>Log in / Registration</p>
           <p>Book table</p>
         </div>
